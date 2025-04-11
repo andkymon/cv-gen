3db5bf6545a7912ce89cf088783c3da37a205908
andkymon <andkymon@gmail.com>
2025-04-11 11:20:51 +0800

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import PersonalInfo from './PersonalInfo.jsx'
 
 function App() {
   return (
     <>
-
+      <div className='left'>
+        <h1>CVGen</h1>
+        <PersonalInfo />
+      </div>
+      <div className='right'></div>
     </>
   )
 }
