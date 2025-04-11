a1bdcb0fdbbcdfbb079b06f1c961cb8b86bf2a2d
andkymon <andkymon@gmail.com>
2025-04-11 17:15:51 +0800

    default state for personalInfo / Preview styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,29 @@ import PersonalInfo from './PersonalInfo.jsx'
 import Education from './Education.jsx'
 import Work from './Work.jsx'
 import Preview from './Preview.jsx'
+import { useState } from 'react'
+
+const defaultPersonalInfo = {
+  name: 'John Doe',
+  email: 'johndoe@email.com',
+  contact: '0987654321',
+  linkedin: 'linkedin.com/john-doe-12345',
+  website: 'johndoe.com',
+}
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({ ...defaultPersonalInfo });
+
   return (
     <>
       <div className='left'>
         <h1>CVGen</h1>
-        <PersonalInfo />
+        <PersonalInfo onSave={setPersonalInfo}/>
         <Education />
         <Work />
       </div>
       <div className='right'>
-        <Preview />
+        <Preview personalInfo={personalInfo}/>
       </div>
     </>
   )
--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,11 +1,24 @@
-import './Preview.css'
+import "./Preview.css";
 
-function Preview() {
+function Preview({ personalInfo }) {
   return (
     <div className="preview">
-        Hi
+      <h1>{personalInfo.name}</h1>
+      <div className="contact-info">
+        {personalInfo.email}
+        {(personalInfo.email.replace(/\s/g, "").length > 0 &&
+          personalInfo.contact.replace(/\s/g, "").length > 0) &&
+          " | "}
+        {personalInfo.contact}
+        <br></br>
+        {personalInfo.linkedin}
+        {(personalInfo.linkedin.replace(/\s/g, "").length > 0 &&
+          personalInfo.website.replace(/\s/g, "").length > 0) &&
+          " | "}
+        {personalInfo.website}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Preview
+export default Preview;
