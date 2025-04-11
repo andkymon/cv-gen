a4c299ffdf113a4a257748a7a32576428475e8ed
andkymon <andkymon@gmail.com>
2025-04-11 14:30:51 +0800

    Remove unnecessary section class name

--- src/PersonalInfo.jsx
+++ src/PersonalInfo.jsx
@@ -2,13 +2,13 @@ import Input from "./Input.jsx"
 
 function PersonalInfo() {
   return (
-    <section className='personal-info'>
+    <section>
       <h2>Personal Information</h2>
-      <Input id='name' label='Name:' type='text' />
-      <Input id='email' label='Email:' type='email' />
-      <Input id='contact' label='Contact Number:' type='tel' />
-      <Input id='linkedin' label='LinkedIn:' type='text' />
-      <Input id='website' label='Website:' type='text' />
+      <Input id='name' label='Name:'/>
+      <Input id='email' label='Email:' type='email'/>
+      <Input id='contact' label='Contact Number:' type='tel'/>
+      <Input id='linkedin' label='LinkedIn:'/>
+      <Input id='website' label='Website:'/>
       <button className='save'>Save</button>
     </section>
   )
