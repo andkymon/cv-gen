a33057cbb936cb1f6cf5a8da68100eb061b834cb
andkymon <andkymon@gmail.com>
2025-04-12 10:42:14 +0800

    Setup education state and props

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,22 +1,58 @@
 import Input from "./Input.jsx"
+import { useState } from 'react'
 
-function Education() {
+function Education({ data, setData }) {
+  const [showForm, setShowForm] = useState([true, 'Hide']);
+  
+  const toggleFormVisibility = () => {
+    setShowForm(!showForm[0] ? [true, 'Hide'] : [false, 'Edit']);
+  }
+
   return (
     <section>
-      <h2>Education</h2>
-      <Input id='degree' label='Degree:' />
-      <Input id='school' label='School:' />
-      <div className='date-wrapper'>
-        <Input id='school-start' label='Start Date:' type='date' />
-        <Input id='school-end' label='End Date:' type='date' />
+      <div className='heading-wrapper'>
+        <h2>Education</h2>
+        <button className='dropdown-button' onClick={toggleFormVisibility}>{showForm[1]}</button>
       </div>
-      <Input id='school-description' label='Description:' isTextbox={true} />
-      <div className='button-wrapper'>
-        <button>Add</button>
-        <button>Save</button>
-      </div>
+      {showForm[0] && <EducationForm data={data} setData={setData}/>}
     </section>
   )
 }
+
+function EducationForm( { data, setData } ) {
+  const [degreeValue, setDegreeValue] = useState(data.degree);
+  const [schoolValue, setSchoolValue] = useState(data.school);
+  const [startDateValue, setStartDateValue] = useState(data.start_date);
+  const [endDateValue, setEndDateValue] = useState(data.end_date);
+  const [descriptionValue, setDescriptionValue] = useState(data.description);
+
+  const onSave = () => {
+    setData(
+      { 
+        degree: degreeValue,
+        school: schoolValue,
+        start_date: startDateValue,
+        end_date: endDateValue,
+        description: descriptionValue,
+      }
+    );
+  }
+
+  return(
+    <>
+      <Input label='Degree:' value={degreeValue} setState={setDegreeValue}/>
+      <Input label='School:' value={schoolValue} setState={setSchoolValue}/>
+      <div className='date-wrapper'>
+        <Input label='Start Date:' type='date' value={startDateValue} setState={setStartDateValue}/>
+        <Input label='End Date:' type='date' value={endDateValue} setState={setEndDateValue}/>
+      </div>
+      <Input label='Description:' isTextbox={true} value={descriptionValue} setState={setDescriptionValue}/>
+      <div className='button-wrapper'>
+        <button onClick={onSave}>Save</button>
+      </div>
+    </>
+  )
+}
+
 
 export default Education
