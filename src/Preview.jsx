3fd3d7a3ea525a3f41f5cc104e34fc4b169299d5
andkymon <andkymon@gmail.com>
2025-04-12 11:19:37 +0800

    Style preview for education and work experience

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,9 +1,12 @@
+import { format } from "date-fns";
 import "./Preview.css";
 
-function Preview({ personalInfo }) {
+function Preview({ personalInfo, education, work }) {
   return (
     <div className="preview">
       <PersonalInfoCV personalInfo={personalInfo} />
+      <EducationCV education={education} />
+      <WorkCV work={work} />
     </div>
   );
 }
@@ -25,6 +28,34 @@ function PersonalInfoCV({ personalInfo }) {
           " | "}
         {personalInfo.website}
       </div>
+    </>
+  )
+}
+
+function EducationCV({ education }) {
+  return (
+    <>
+      <h2>Education</h2>
+      <div class='heading-wrapper'>
+        <h3>{education.degree}</h3>
+        <div>{format(education.start_date, 'MMMM y')} - {format(education.end_date, 'MMMM y')}</div>
+      </div>
+      <div>{education.school}</div>
+      <p>◦ {education.description}</p>
+    </>
+  )
+}
+
+function WorkCV({ work }) {
+  return (
+    <>
+      <h2>Work Experience</h2>
+      <div class='heading-wrapper'>
+        <h3>{work.position}</h3>
+        <div>{format(work.start_date, 'MMMM y')} - {format(work.end_date, 'MMMM y')}</div>
+      </div>
+      <div>{work.company}</div>
+      <p>◦ {work.description}</p>
     </>
   )
 }
