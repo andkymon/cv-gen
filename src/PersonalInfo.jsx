c27856306b31bbb752013da92edba53e2b863b90
andkymon <andkymon@gmail.com>
2025-04-12 09:36:56 +0800

    Implement dropdown/edit button for personal info

--- src/PersonalInfo.jsx
+++ src/PersonalInfo.jsx
@@ -2,6 +2,24 @@ import Input from "./Input.jsx"
 import { useState } from 'react'
 
 function PersonalInfo({ data, setData }) {
+  const [showForm, setShowForm] = useState([true, 'Hide']);
+
+  const toggleFormVisibility = () => {
+    setShowForm(!showForm[0] ? [true, 'Hide'] : [false, 'Edit']);
+  }
+
+  return (
+    <section>
+      <div className='heading-wrapper'>
+        <h2>Personal Information</h2>
+        <button className='dropdown-button' onClick={toggleFormVisibility}>{showForm[1]}</button>
+      </div>
+      {showForm[0] && <PersonalInfoForm data={data} setData={setData}/>}
+    </section>
+  )
+}
+
+function PersonalInfoForm( { data, setData } ) {
   const [nameValue, setNameValue] = useState(data.name);
   const [emailValue, setEmailValue] = useState(data.email);
   const [contactValue, setContactValue] = useState(data.contact);
@@ -37,8 +55,7 @@ function PersonalInfo({ data, setData }) {
   }
 
   return (
-    <section>
-      <h2>Personal Information</h2>
+    <>
       <Input label='Name:' value={nameValue} onChange={nameOnChange}/>
       <Input label='Email:' value={emailValue} onChange={emailOnChange}/>
       <Input label='Contact Number:' value={contactValue} onChange={contactOnChange} type='tel'/>
@@ -47,8 +64,9 @@ function PersonalInfo({ data, setData }) {
       <div className='button-wrapper'>
         <button onClick={onSave}>Save</button>
       </div>
-    </section>
+    </>
   )
 }
+
 
 export default PersonalInfo
